0c2d8d5d81e6bbd35582334f6b8d563e25026a21
Ciprian <recianu.ciprian@gmail.com>
2018-04-10 01:09:28 +0300

    latest config before start building app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = <div> test </div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
